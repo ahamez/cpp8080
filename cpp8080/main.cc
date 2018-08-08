90cc901ee1f4dd61178f1762fed9e2d6d08b0359
Alexandre Hamez <alexandre.hamez@gmail.com>
2018-08-08 20:52:13 +0200

    Move Space Invaders rom into a `bin` directory

--- cpp8080/main.cc
+++ cpp8080/main.cc
@@ -14,18 +14,18 @@ main(int argc, char** argv)
 {
   if (argc != 2)
   {
-    std::cerr << "Usage: " << argv[0] << " /path/to/rom_file\n";
+    std::cerr << "Usage: " << argv[0] << " /path/to/file\n";
     return 1;
   }
-  auto rom_file = std::ifstream{argv[1], std::ios::binary};
-  if (not rom_file.is_open())
+  auto file = std::ifstream{argv[1], std::ios::binary};
+  if (not file.is_open())
   {
     std::cerr << "Cannot open ROM file " << argv[1] << '\n';
     return 1;
   }
 
   auto machine =
-    std::make_shared<cpp8080::machine::space_invaders>(std::istreambuf_iterator<char>{rom_file},
+    std::make_shared<cpp8080::machine::space_invaders>(std::istreambuf_iterator<char>{file},
                                                        std::istreambuf_iterator<char>{});
   auto emulator = cpp8080::emulator{machine};
 
